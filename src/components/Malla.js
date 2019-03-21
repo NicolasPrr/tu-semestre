cab26fa6e645ba1a1411bb362a6075dd9df4531d
nicolasprr <niparrara@unal.edu.co>
2019-03-21 18:00:41 -0500

    plot average tipology

--- src/components/Malla.js
+++ src/components/Malla.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import { LineChart, Line, CartesianGrid, XAxis, YAxis , Tooltip, ReferenceLine} from 'recharts';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, ReferenceLine } from 'recharts';
+import {
+    Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis,
+} from 'recharts'
 
 const RenderLineChart = (props) => {
     return (
@@ -10,7 +13,7 @@ const RenderLineChart = (props) => {
                 <CartesianGrid strokeDasharray="3 3" />
                 <ReferenceLine y={3} label="min" stroke="red" />
                 <ReferenceLine y={5} label="max" stroke="blue" />
-              
+
                 <XAxis dataKey="name" />
                 <YAxis />
                 <Tooltip />
@@ -19,7 +22,19 @@ const RenderLineChart = (props) => {
         </div>
     );
 };
-
+const RenderRadar = (props) => {
+    
+    return (
+        <RadarChart cx={300} cy={250} outerRadius={150} width={500} height={500} data={props.data}>
+        <PolarGrid />
+        <PolarAngleAxis dataKey="name" />
+        <PolarRadiusAxis />
+        <Radar name="Promedio tipologia" dataKey="average" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
+        <Tooltip/>
+        
+    </RadarChart>
+    )
+}
 const data = [{ name: 'Page A', uv: 400, pv: 2400, amt: 2400 }, { name: 'Page B', uv: 500, pv: 240, amt: 2400 }, { name: 'Page c', uv: 200, pv: 240, amt: 2400 }];
 
 class Malla extends Component {
@@ -28,6 +43,7 @@ class Malla extends Component {
         this.state = {
             years: [],
             periods: [],
+            dataRadar: []
         };
         this.calculatePAPA = this.calculatePAPA.bind(this);
     }
@@ -85,6 +101,75 @@ class Malla extends Component {
             </div>
         );
     };
+    averagePerTopology(periods) {
+        var courses;
+
+        var fundamentation;
+        var disciplinar;
+        var elective;
+
+        var creditsL = 0;
+        var scorePerCredtis_L = 0;
+
+        var creditsOF = 0;
+        var scorePerCredtis_OF = 0;
+
+        var creditsOF = 0;
+        var scorePerCredtis_OF = 0;
+
+        var creditsOPF = 0;
+        var scorePerCredtis_OPF = 0;
+
+        var creditsOD = 0;
+        var scorePerCredtis_OD = 0;
+
+        var creditsOPD = 0;
+        var scorePerCredtis_OPD = 0;
+        //B FUNDAMENTAL OBLIGATORIA
+        //O FUNDAMENTAL OPTATIVA
+
+        //C DISCIPLINAR OBLIGATORIA
+        //T DISCIPLINAR OPTATIVA
+
+        //L LIBRE ELECCIÓN
+        //TRABAJO DE GRADO
+        console.log(periods);
+        for (var i = 0; i < periods.length; i++) {
+            courses = periods[i].courses;
+            for (var j = 0; j < courses.length; j++) {
+                if (courses[j][7] === 'L') {
+                    scorePerCredtis_L += courses[j][8] * parseFloat(courses[j][10]);
+                    creditsL += parseInt(courses[j][8]);
+                }
+
+                if (courses[j][7] === 'B') {
+                    scorePerCredtis_OF += courses[j][8] * parseFloat(courses[j][10]);
+                    creditsOF += parseInt(courses[j][8]);
+                }
+                if (courses[j][7] === 'O') {
+                    scorePerCredtis_OPF += courses[j][8] * parseFloat(courses[j][10]);
+                    creditsOPF += parseInt(courses[j][8]);
+                }
+
+                if (courses[j][7] === 'C') {
+                    scorePerCredtis_OD += courses[j][8] * parseFloat(courses[j][10]);
+                    creditsOD += parseInt(courses[j][8]);
+                }
+                if (courses[j][7] === 'T') {
+                    scorePerCredtis_OPD += courses[j][8] * parseFloat(courses[j][10]);
+                    creditsOPD += parseInt(courses[j][8]);
+                }
+
+            }
+            console.log(creditsL)
+        }
+        fundamentation = ((scorePerCredtis_OF + scorePerCredtis_OPF) / (creditsOF + creditsOPF)).toFixed(2);
+        disciplinar = ((scorePerCredtis_OD + scorePerCredtis_OPD) / (creditsOD + creditsOPD)).toFixed(2)
+        elective = scorePerCredtis_L / creditsL;
+
+        return [fundamentation, disciplinar, elective];
+
+    }
 
     componentDidMount() {
         var periods = [];
@@ -93,6 +178,13 @@ class Malla extends Component {
         var creditsPerScore = 0;
         var totalCredits = 0
         var result;
+        var fundamentation;
+        var disciplinar;
+        var elective;
+        var radar;
+        //nota por tipologia (Electiva, obligatoria fundamental, optativa fundamental, disciplinar obligatoria y optativa disciplinar)
+        //calculo del papi, pappa
+
         for (var i = 0; i < periods.length; i++) {
             const PAPPI = this.calculatePAPPI(periods[i].courses);
             result = this.calculatePAPA(periods[i].courses, creditsPerScore, totalCredits)
@@ -101,6 +193,15 @@ class Malla extends Component {
             creditsPerScore = result[1];
             totalCredits = result[2];
         }
+
+        [fundamentation, disciplinar, elective] = this.averagePerTopology(periods);
+
+        radar = [
+            { name: "fundamentacion", average: fundamentation },
+            { name: "disciplinar", average: disciplinar },
+            { name: "electiva", average: elective }
+        ]
+        this.setState({ dataRadar: radar })
         this.setState({ periods: periods })
         for (i = 0; i < periods.length; i++) {
             if (years.length === 0 || periods[i].name.split("-")[0] !== years[years.length - 1].year) {
@@ -152,7 +253,9 @@ class Malla extends Component {
 
                 </div>
                 <div className="column">
-                    {<RenderLineChart data = {this.state.periods} />}
+                    <RenderLineChart data={this.state.periods} />
+                    <RenderRadar data= {this.state.dataRadar}/>
+
                 </div>
             </div>
         );
