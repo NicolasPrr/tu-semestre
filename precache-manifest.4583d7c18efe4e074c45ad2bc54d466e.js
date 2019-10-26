80fe342e74cbff5e58d4870f92573a7cfe6ac362
=nicolasprr <niparrar@unal.edu.co>
2019-10-25 22:34:21 -0500

    Updates

---
+++ precache-manifest.4583d7c18efe4e074c45ad2bc54d466e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0aa3f9003fcd9f31990",
+    "url": "/tu-semestre/static/js/runtime~main.e15fa729.js"
+  },
+  {
+    "revision": "da83dbe2d094031fa9ed",
+    "url": "/tu-semestre/static/js/main.45d44379.chunk.js"
+  },
+  {
+    "revision": "b2ee12ce6a6ba7cade61",
+    "url": "/tu-semestre/static/js/2.ca820ab5.chunk.js"
+  },
+  {
+    "revision": "da83dbe2d094031fa9ed",
+    "url": "/tu-semestre/static/css/main.7b7865cf.chunk.css"
+  },
+  {
+    "revision": "b2ee12ce6a6ba7cade61",
+    "url": "/tu-semestre/static/css/2.7c06e961.chunk.css"
+  },
+  {
+    "revision": "0b770b48cd478af5e203fab5ba4c2a47",
+    "url": "/tu-semestre/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/TuSemestre/precache-manifest.6575c25dfea07520e4664da8edc06674.js"
+  "/tu-semestre/precache-manifest.4583d7c18efe4e074c45ad2bc54d466e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/TuSemestre/index.html", {
+workbox.routing.registerNavigationRoute("/tu-semestre/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
