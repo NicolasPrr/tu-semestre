da7dad3e9c0f0f325981ad7b0c73707b2ea6e19e
=nicolasprr <niparrar@unal.edu.co>
2019-12-20 00:56:04 -0500

    Updates

---
+++ precache-manifest.6db1166565ee04d2bcf15612a76794ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "b78b9cba3c71075b27d0",
+    "url": "./static/js/main.20c2c943.chunk.js"
+  },
+  {
+    "revision": "b2ee12ce6a6ba7cade61",
+    "url": "./static/js/2.ca820ab5.chunk.js"
+  },
+  {
+    "revision": "b78b9cba3c71075b27d0",
+    "url": "./static/css/main.7b7865cf.chunk.css"
+  },
+  {
+    "revision": "b2ee12ce6a6ba7cade61",
+    "url": "./static/css/2.7c06e961.chunk.css"
+  },
+  {
+    "revision": "eef98ba54b6fe279c89acfd11cc30a79",
+    "url": "./index.html"
+  }
+];
